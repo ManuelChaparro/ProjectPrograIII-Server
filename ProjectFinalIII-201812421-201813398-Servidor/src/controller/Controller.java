f5f92c033ed5f4f0bf8596998ab0dcf908c14c39
LuisFernandoSandovalParra <luis.sandoval03@uptc.edu.co>
2021-04-25 20:26:32 -0500

    Añadido clases para realizar conexion con clientes (developerLucho).

--- ProjectFinalIII-201812421-201813398-Servidor/src/controller/Controller.java
+++ ProjectFinalIII-201812421-201813398-Servidor/src/controller/Controller.java
@@ -1,12 +1,28 @@
 package controller;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Controller {
     
-	private ServerSocket serverSocket;
-	private String PORT;
+	private ServerSocket server;
+	private static final int PORT =24211;
 	
 	public Controller() {
+		try {
+			server = new ServerSocket(PORT);
+			Logger.getGlobal().log(Level.INFO, "Arranco el servidor.");
+			while(true) {
+				Socket socket = server.accept();
+				new Client(socket).start();
+				Logger.getGlobal().log(Level.INFO, "Nueva conexion aceptada");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			Logger.getGlobal().log(Level.WARNING, "Servidor ya se encuentra corriendo.");
+		}
 	}
 }
